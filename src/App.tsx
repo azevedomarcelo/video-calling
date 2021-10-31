101946652d4f3a5a95554c7cf8566829ff6a82cc
marcelo.azevedo <marcelo.azevedo@connectcorp.com.br>
2021-10-31 17:56:12 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { RiPencilFill } from 'react-icons/ri';
+
+import { data } from './config';
+
 import './App.css';
 
 function App() {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const [showModal, setShowModal] = useState(false)
+  const [avatarImage, setAvatarImage] = useState(data[0].logo)
+  const [nickname, setNickname] = useState('')
+
+  const showModalState = () => {
+    setShowModal(!showModal);
+  }
+
+  const handleChangeAvatar = () => {
+    setAvatarImage(data[activeImageIndex].logo);
+    showModalState();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1 className="title-page">Escolha um nome e um ícone</h1>
+      <div className="avatar-input">
+        <img id="avatar" src={avatarImage} alt="avatar" />
+        <div className="avatar" onClick={showModalState}>
+          <RiPencilFill />
+        </div>
+      </div>
+
+      <input
+        type="text"
+        name="nickname"
+        className="input-name"
+        id="nickname"
+        placeholder="Insira seu nome"
+        onChange={(e) => setNickname(e.target.value)}
+        value={nickname}
+      />
+
+      {showModal && (
+        <section className="modal-body">
+          <h1 className="title-modal">AVATAR</h1>
+          <div className="modal-image">
+            {data.map((image, index) => (
+              <img
+                key={image.id}
+                src={image.logo}
+                alt={image.alt}
+                className={
+                  activeImageIndex === index ? "image active" : "image"
+                }
+                onClick={() => setActiveImageIndex(index)}
+              />
+            ))}
+          </div>
+          <button className="buttonConfirm" onClick={handleChangeAvatar}>
+            CONFIRMAR
+          </button>
+        </section>
+      )}
+
+      <button
+        className="buttonEnter"
+        onClick={() => alert(`Bem vindo(a) ${nickname}`)}
+      >
+        ENTRAR
+      </button>
     </div>
   );
 }
---
+++ src/config.ts
@@ -0,0 +1,75 @@
+import icon1 from './assets/icons/Icon-1.svg';
+import icon2 from './assets/icons/Icon-2.svg';
+import icon3 from './assets/icons/Icon-3.svg';
+import icon4 from './assets/icons/Icon-4.svg';
+import icon5 from './assets/icons/Icon-5.svg';
+import icon6 from './assets/icons/Icon-6.svg';
+import icon7 from './assets/icons/Icon-7.svg';
+import icon8 from './assets/icons/Icon-8.svg';
+import icon9 from './assets/icons/Icon-9.svg';
+import icon10 from './assets/icons/Icon-10.svg';
+import icon11 from './assets/icons/Icon-11.svg';
+import icon12 from './assets/icons/Icon-12.svg';
+
+export const data = [
+  {
+    "id": 1,
+    "alt": "icon-1",
+    "logo": icon1
+  },
+  {
+    "id": 2,
+    "alt": "icon-2",
+    "logo": icon2
+  },
+  {
+    "id": 3,
+    "alt": "icon-3",
+    "logo": icon3
+  },
+  {
+    "id": 4,
+    "alt": "icon-4",
+    "logo": icon4
+  },
+  {
+    "id": 5,
+    "alt": "icon-5",
+    "logo": icon5
+  },
+  {
+    "id": 6,
+    "alt": "icon-6",
+    "logo": icon6
+  },
+  {
+    "id": 7,
+    "alt": "icon-7",
+    "logo": icon7
+  },
+  {
+    "id": 8,
+    "alt": "icon-8",
+    "logo": icon8
+  },
+  {
+    "id": 9,
+    "alt": "icon-9",
+    "logo": icon9
+  },
+  {
+    "id": 10,
+    "alt": "icon-10",
+    "logo": icon10
+  },
+  {
+    "id": 11,
+    "alt": "icon-11",
+    "logo": icon11
+  },
+  {
+    "id": 12,
+    "alt": "icon-12",
+    "logo": icon12
+  }
+]
